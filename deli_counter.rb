f63ca36cf2fb9901919e154d84cb657657d02452
Shell <h.k.domingo@outlook.com>
2022-04-18 14:27:58 +0800

    “Done”

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,5 @@
 # Write your code here.
+
 def take_a_number(katz_deli, name)
     katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
@@ -28,3 +29,4 @@ def take_a_number(katz_deli, name)
   
   
   end
+  
